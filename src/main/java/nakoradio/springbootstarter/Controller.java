525ec88004e2a079d874d3525c332e79cde1d407
Mikko Ravimo <mrav@FL911.home>
2017-10-27 19:15:13 +0300

    Print profile

--- src/main/java/nakoradio/springbootstarter/Controller.java
+++ src/main/java/nakoradio/springbootstarter/Controller.java
@@ -1,14 +1,24 @@
 package nakoradio.springbootstarter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @RestController
 public class Controller {
 
+    @Autowired
+    private Environment environment;
+
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot profile: " +
+                Arrays.stream(environment.getActiveProfiles()).collect(Collectors.joining(","));
     }
 
 }
